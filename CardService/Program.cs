b9781d8e6c573b498fb38b2923d78c397f4ff116
DUONG Phu-Hiep <hduong@lemonway.com>
2024-03-19 09:36:30 +0100

    okey!

--- CardService/Program.cs
+++ CardService/Program.cs
@@ -9,8 +9,8 @@ var appActivitySource = new ActivitySource(app.Environment.ApplicationName);
 app.MapPost("/api/createCardPayment", async ([FromServices] ILogger<Program> logger, [FromServices] HttpClient httpClient) =>
 {
     logger.LogInformation("start createCardPayment");
-    await httpClient.GetStringAsync("https://httpstat.us/200?sleep=1000");
-    await httpClient.GetStringAsync("https://httpstat.us/301"); //this will make 2 https requests to return the response because the target URI returns a redirection HTTP 301
+    var r = await httpClient.PostAsJsonAsync("https://transactionService/api/CreateTransaction", "{}");
+    logger.LogInformation("success call transaction service {result}", r);
     logger.LogInformation("end createCardPayment");
     return "OK";
 });
--- TransactionService/Program.cs
+++ TransactionService/Program.cs
@@ -9,8 +9,8 @@ var appActivitySource = new ActivitySource(app.Environment.ApplicationName);
 app.MapPost("/api/createTransaction", async ([FromServices] ILogger<Program> logger, [FromServices] HttpClient httpClient) =>
 {
     logger.LogInformation("start createTransaction");
-    await httpClient.GetStringAsync("https://httpstat.us/200?sleep=1000");
-    await httpClient.GetStringAsync("https://httpstat.us/301"); //this will make 2 https requests to return the response because the target URI returns a redirection HTTP 301
+    //await httpClient.GetStringAsync("https://httpstat.us/200?sleep=1000");
+    //await httpClient.GetStringAsync("https://httpstat.us/301"); //this will make 2 https requests to return the response because the target URI returns a redirection HTTP 301
     logger.LogInformation("end createTransaction");
     return "OK";
 });
